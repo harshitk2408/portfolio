eb85892162a2516090dfda8e7b9955a0b0811f3d
harshitk2408 <khurana.harshit2003@gmail.com>
2023-05-25 02:25:55 +0530

    Final to be uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import Education from './components/Education/Education'
 import Contact from './components/Contact/Contact'
 import Header from './components/Header/Header'
 import Projects from './components/Projects/Projects'
+// import Particle from './components/Particle/Particle'
 export default function App() {
     return (
         <div>
+            {/* <Particle/> */}
             <Header />
             <Navbar />
             <Me />
